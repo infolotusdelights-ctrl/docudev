ad9e44ce215b238a7aa2bf205d926647727039e6
infolotusdelights-ctrl <info.lotusdelights@gmail.com>
2026-02-04 13:51:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you! DocuDev will contact you soon 🚀");
+    this.reset();
+});
